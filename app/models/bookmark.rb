68f7a1f49957edc0793e314c1f14aa372cd2d3d0
Karl von der Eltz <karlvondereltz@gmail.com>
2021-11-11 15:02:45 +0100

    totally on time

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,9 @@
 class Bookmark < ApplicationRecord
   belongs_to :list
   belongs_to :movie
+
+  validates_associated :list, :movie
+  validates :movie, uniqueness: { scope: :list }
+
+  validates :comment, length: { minimum: 6 }
 end
